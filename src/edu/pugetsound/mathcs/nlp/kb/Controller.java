e1891112e085e5516137fc3c017f5d875e00ec3f
aingerson <aingerson@pugetsound.edu>
2016-03-23 11:51:43 -0700

    Now this compiles - sorry.

--- src/edu/pugetsound/mathcs/nlp/kb/Controller.java
+++ src/edu/pugetsound/mathcs/nlp/kb/Controller.java
@@ -5,12 +5,15 @@ import gnu.prolog.vm.*;
 import gnu.prolog.vm.buildins.database.*;
 
 public class Controller{
+  Environment env;
+  Interpreter interpreter;
 
-  Environment env = new Environment();
-  env.ensureLoaded(AtomTerm.get(Test.class.getResource("knowledge/cats.pl").getFile()));
-  Interpreter interpreter = env.createInterpreter();
-  env.runInitialization(interpreter);
-
+  public Controller(){
+    env = new Environment();
+    env.ensureLoaded(AtomTerm.get(Controller.class.getResource("knowledge/cats.pl").getFile()));
+    interpreter = env.createInterpreter();
+    env.runInitialization(interpreter);
+  }
 
 /*
 * User called yes/no query method
@@ -23,37 +26,41 @@ public class Controller{
     Interpreter interpret = interpreter;
 
 
-
-
-    int rc = runQuery(interpret, pred,args);
-    if (rc == PrologCode.SUCCESS || rc == PrologCode.SUCCESS_LAST){
-      return true;
+    try{
+      int rc = runQuery(interpret, pred,args);
+      if (rc == PrologCode.SUCCESS || rc == PrologCode.SUCCESS_LAST){
+        return true;
+      }
+      return false;
+    }
+    catch(PrologException e){
+      //TODO properly catch this
     }
     return false;
   }
 
   //method handling queries internally using a specific interpreter (picking which file to query)
   private static int runQuery(Interpreter interpreter, String pred, String[] queryArgs)  throws PrologException {
-      Term[] terms = new Term[queryArgs.length];
-      for(int i=0;i<queryArgs.length;i++){
-        terms[i] = AtomTerm.get(queryArgs[i]);
-      }
-      CompoundTerm goalTerm = new CompoundTerm(AtomTerm.get(pred), terms);
-      int rc = interpreter.runOnce(goalTerm);
-      return rc;
+    Term[] terms = new Term[queryArgs.length];
+    for(int i=0;i<queryArgs.length;i++){
+      terms[i] = AtomTerm.get(queryArgs[i]);
+    }
+    CompoundTerm goalTerm = new CompoundTerm(AtomTerm.get(pred), terms);
+    int rc = interpreter.runOnce(goalTerm);
+    return rc;
   }
-}
 
-//method handling knowledge assertion using specific interpreter
-private static int assertKnowledge(Interpreter interpreter, String pred, String[] assertArgs) throws PrologException{
-  //TODO this method doesn't actually work... fix it
+  //method handling knowledge assertion using specific interpreter
+  private static int assertKnowledge(Interpreter interpreter, String pred, String[] assertArgs) throws PrologException{
+    //TODO this method doesn't actually work... fix it
 
-  Term[] terms = new Term[assertArgs.length+1];
-  terms[0] = AtomTerm.get(pred);
-  for(int i=0;i<assertArgs.length;){
-    terms[i+1] = AtomTerm.get(assertArgs[i]);
+    Term[] terms = new Term[assertArgs.length+1];
+    terms[0] = AtomTerm.get(pred);
+    for(int i=0;i<assertArgs.length;){
+      terms[i+1] = AtomTerm.get(assertArgs[i]);
+    }
+    Predicate_assert asserter = new Predicate_assertz();
+    int rc = asserter.execute(interpreter,false,terms);
+    return rc;//returns whether successful
   }
-  Predicate_assert pred = new Predicate_assertz();
-  int rc = pred.execute(interpreter,false,terms);
-  return rc;//returns whether successful
 }
