59568437563482312a3a89685b13078f71f2d428
ledman1 <ledman@pugetsound.edu>
2016-03-23 11:12:20 -0700

    all classes added

---
+++ src/edu/pugetsound/mathcs/nlp/datag/SwitchboardParser.java
@@ -0,0 +1,10 @@
+package edu.pugetsound.mathcs.nlp.datag;
+
+public class SwitchboardParser {
+	/**
+	* Given a path name, parses through the file and returns the list of DATags associated with each entry.
+	**/
+	public static List<DATag> parse(String path){
+		return null;
+	}
+}
