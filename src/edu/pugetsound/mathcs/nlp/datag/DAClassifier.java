894a23a2475b949c7d5f573febb98622ba4e4d8b
ChiliJohnson <heroku@chilj.com>
2016-03-25 01:12:41 -0700

    Adds dummy classification.

--- src/edu/pugetsound/mathcs/nlp/datag/DAClassifier.java
+++ src/edu/pugetsound/mathcs/nlp/datag/DAClassifier.java
@@ -1,5 +1,17 @@
 package edu.pugetsound.mathcs.nlp.datag;
 
+import edu.pugetsound.mathcs.nlp.lang.Conversation;
+import edu.pugetsound.mathcs.nlp.lang.Utterance;
+
 public class DAClassifier {
 
+	public DAClassifier() {
+		
+	}
+	
+	public DialogueActTag classify(Utterance utterance, Conversation conversation) {
+		int randIndex = (int)(Math.random() * DialogueActTag.values().length);
+		return DialogueActTag.values()[randIndex];
+	}
+	
 }
