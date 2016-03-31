5bac72005fbfac2e63e0f74986d6f8b3fdb2ff4a
Damon Williams <dgwilliams@pugetsound.edu>
2016-03-30 17:09:46 -0700

    Q learner adapted

--- src/edu/pugetsound/mathcs/nlp/mdp/State.java
+++ src/edu/pugetsound/mathcs/nlp/mdp/State.java
@@ -4,17 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import edu.pugetsound.mathcs.nlp.datag.DialogueActTag;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Zachary Cohan
- */
-
 public class State {
 //        int id;
         DialogueActTag DATag;
