9486a228ac2055d514d06015b61b6bd1d74005b4
ThomasGagne <thomasgagne100@gmail.com>
2016-04-19 17:04:20 -0700

    Moved AMR.SemanticRelation into its own class.

--- src/edu/pugetsound/mathcs/nlp/lang/AMR.java
+++ src/edu/pugetsound/mathcs/nlp/lang/AMR.java
@@ -16,6 +16,7 @@ import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
 import edu.pugetsound.mathcs.nlp.lang.Token;
+import edu.pugetsound.mathcs.nlp.lang.SemanticRelation;
 
 /**
  * Represents a filled-in AMR node
@@ -30,106 +31,13 @@ public class AMR {
 
     public AMR(){};
 
-    /**
-     * A list of most of the semantic roles in AMR
-     * Current list (used below) is version 1.2.2, updated as of September 18, 2015
-     * Source: https://github.com/amrisi/amr-guidelines/blob/master/amr.md#part-ii--concepts-and-relations
-     */
-    public enum SemanticRelation {
-        // align role, for consistency with msrsplat
-        align("align"),
-
-    	// Core argX roles, following OntoNotes style
-        arg0("arg0"), arg1("arg1"), arg2("arg2"), arg3("arg3"), arg4("arg4"), arg5("arg5"),
-
-        //Non-core roles
-        accompanier("accompanier"), age("age"), beneficiary("beneficiary"),
-        compared_to("compared_to"), concession("concession"), condition("condition"),
-        consist_of("consist_of"), degree("degree"), destination("destination"),
-        direction("direction"), domain("domain"), duration("duration"), example("example"),
-        extent("extent"), frequency("frequency"), instrument("instrument"), location("location"),
-        manner("manner"), medium("medium"), mod("mod"), mode("mode"), name("name"), ord("ord"),
-        part("part"), path("path"), polarity("polarity"), poss("poss"), purpose("purpose"),
-        quant("quant"), scale("scale"), source("source"), subevent("subevent"), time("time"),
-        topic("topic"), unit("unit"), value("value"), wiki("wiki"),
-
-        // Date-entity roles
-        calendar("calendar"), century("century"), day("day"), dayperiod("dayperiod"), decade("decade"),
-        era("era"), month("month"), quarter("quarter"), season("season"), timezone("timezone"),
-        weekday("weekday"), year("year"), year2("year2"),
-
-        // Used in conjunctions and certain date-times and locations. Might not be used
-        op1("op1"), op2("op2"), op3("op3"), op4("op4"), op5("op5"),
-        op6("op6"), op7("op7"), op8("op8"), op9("op9"), op10("op10"),
-
-        // Note that we lack any prep-X roles for the time being, due to ambiguity
-        // For an example, a partial list is given as following:
-        //
-        // prep_against, prep_along_with, prep_amid, prep_among, prep_as, prep_at,
-        // prep_by,
-        // prep_for, prep_from,
-        // prep_in, prep_in_addition_to, prep_into,
-        // prep_on, prep_on_behalf_of, prep_out_of,
-        // prep_to, prep_toward,
-        // prep_under,
-        // prep_with, prep_without,
-
-        // Some conjunctions are also not well-covered under the list of non-core roles.
-        // AMR also likes to avoid these, but sometimes we have no good alternative.
-        // We lack any conjunctions outside the list of non-core roles for now, due to ambiguity.
-        //
-        // e.g. of what one might be: conj_as_if
-
-        // Inverse relations
-        // Core argX roles, following OntoNotes style
-        arg0_of("arg0-of"), arg1_of("arg1-of"), arg2_of("arg2-of"), arg3_of("arg3-of"),
-        arg4_of("arg4-of"), arg5_o("arg5-of"),
-
-        //Non_core roles
-        accompanier_of("accompanier-of"), age_of("age-of"), beneficiary_of("beneficiary-of"),
-        compared_to_of("compared_to-of"), concession_of("concession-of"),
-        condition_of("condition-of"),consist_of_of("consist_of-of"), degree_of("degree-of"),
-        destination_of("destination-of"), direction_of("direction-of"), domain_of("domain-of"),
-        duration_of("duration-of"),example_of("example-of"), extent_of("extent-of"),
-        frequency_of("frequency-of"), instrument_of("instrument-of"), location_of("location-of"),
-        manner_of("manner-of"), medium_of("medium-of"),mod_of("mod-of"), mode_of("mode-of"),
-        name_of("name-of"), ord_of("ord-of"), part_of("part-of"), path_of("path-of"),
-        polarity_of("polarity-of"), poss_of("poss-of"), purpose_of("purpose-of"),
-        quant_of("uant-of"), scale_of("scale-of"), source_of("source-of"), subevent_of("subevent-of"),
-        time_of("time-of"), topic_of("topic-of"), unit_of("unit-of"), value_of("value-of"),
-        wiki_of("wiki-of"),
-
-        // Date_entity roles
-        calendar_of("calendar-of"), century_of("century-of"), day_of("day-of"),
-        dayperiod_of("dayperiod-of"), decade_of("decade-of"), era_of("era-of"),
-        month_of("month-of"), quarter_of("quarter-of"),season_of("season-of"),
-        timezone_of("timezone-of"), weekday_of("weekday-of"), year_of("year-of"), year2_of("year2-of"),
-
-        // Used in conjunctions and certain date_times and locations. Might not be used
-        op1_of("op1-of"), op2_of("op2-of"), op3_of("op3-of"), op4_of("op4-of"), op5_of("op5-of"),
-        op6_of("op6-of"), op7_of("op7-of"), op8_of("op8-of"), op9_of("op9-of"), op10_of("op10-of"),
-
-        // Not sure what this is for, but msrsplat uses it sometimes
-        lf_measure("lf-measure");
-
-        private String label;
-
-        SemanticRelation(String label) {
-            this.label = label;
-        }
-
-        public String getLabel() {
-            return ":" + label;
-        }
-    }
-
     // Used for parsing an AMR string to an object
-    private static final HashMap<String, AMR.SemanticRelation> nameToRelation = new HashMap<String, AMR.SemanticRelation>() {{
-            for(AMR.SemanticRelation sr : AMR.SemanticRelation.values()) {
-                put(sr.label, sr);
+    private static final HashMap<String, SemanticRelation> nameToRelation = new HashMap<String, SemanticRelation>() {{
+            for(SemanticRelation sr : SemanticRelation.values()) {
+                put(sr.getLabel().substring(1), sr);
             }
         }};
-
+    
     /**
      * A list of the possible types an AMR node can be
      * Ex: verb, noun, adjectives (for copulas), possibleness, amr-unknown, etc.
@@ -166,7 +74,7 @@ public class AMR {
     /**
      * The value of each semantic relation, which maps to another AMR or NULL
      */
-    public HashMap<AMR.SemanticRelation, AMR> semanticRelations = new HashMap<AMR.SemanticRelation, AMR>();
+    public HashMap<SemanticRelation, AMR> semanticRelations = new HashMap<SemanticRelation, AMR>();
 
     /**
      * Adds a semantic relation from this AMR to `amr`
@@ -346,7 +254,7 @@ public class AMR {
                 if(in_quote) {
                     AMR quote = new AMR(String.valueOf(cur_charseq.charAt(0)), cur_charseq, AMRType.string);
                     if(state == ':') {
-                        AMR.SemanticRelation sr = AMR.nameToRelation.get(last_word);
+                        SemanticRelation sr = AMR.nameToRelation.get(last_word);
                         if(sr == null) {
                             System.out.println("Error: Unrecognized relation " + last_word  + " while parsing AMR!");
                             return null;
@@ -381,7 +289,7 @@ public class AMR {
                     // Skip over the part of text corresponding to recursed
                     skip_amr = 1;
                     //i++; // Prevent skip_amr from counting the first ( twice
-                    AMR.SemanticRelation sr = AMR.nameToRelation.get(last_word);
+                    SemanticRelation sr = AMR.nameToRelation.get(last_word);
                     if(sr == null) {
                         System.out.println("Error: Unrecognized relation " + last_word  + " while parsing AMR!");
                         return null;
@@ -441,7 +349,7 @@ public class AMR {
                     // The second half of the && is a really quick 'n dirty way of checking that we're
                     // parsing the argument to a relation and not the relation name itself
                     if(state == ':' && text.charAt(i - cur_charseq.length() - 1) != ':') {
-                        AMR.SemanticRelation sr = AMR.nameToRelation.get(last_word);
+                        SemanticRelation sr = AMR.nameToRelation.get(last_word);
                         if(sr == null) {
                             System.out.println("Error: Unrecognized relation " + last_word  + " while parsing AMR!");
                             return null;
