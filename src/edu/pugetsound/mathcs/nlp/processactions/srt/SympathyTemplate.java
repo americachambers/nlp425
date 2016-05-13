e99b93b71edad11682970ceeec29e0f0a2079d20
ThomasGagne <thomasgagne100@gmail.com>
2016-05-13 12:36:47 -0700

    Added Sympathy template

---
+++ src/edu/pugetsound/mathcs/nlp/processactions/srt/SympathyTemplate.java
@@ -0,0 +1,10 @@
+package edu.pugetsound.mathcs.nlp.processactions.srt;
+
+import edu.pugetsound.mathcs.nlp.processactions.srt.SemanticResponseTemplate;
+
+/**
+ * @author Thomas Gagne & Jon Sims
+ * @version 04/26/16
+ * A template for constructing a sympathetic response.
+ */
+public class SympathyTemplate extends SemanticResponseTemplate {}
